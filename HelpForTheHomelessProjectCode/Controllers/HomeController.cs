ccd688718cc57b07df1a6ec80d325ba96aafbc50
Kai <kai.arrowood@konghq.com>
2021-02-05 11:33:18 -0500

    third drop

---
+++ HelpForTheHomelessProjectCode/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpForTheHomelessProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "We Care About Your Needs! We Are Here To Help You!";
+
+            return View();
+        }
+
+        //Rebuild all the time go back and refresh
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Please Contact Our Office at any time!";
+
+            return View();
+        }
+
+        
+    }
+}
